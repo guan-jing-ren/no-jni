e44ecd940d22e766c95fc5228d0dd7353b2814c1
Kwan King Yan <kwan.king.yan@gmail.com>
2017-11-14 01:10:56 +1100

    constexpr signatures for primitive types.

--- no-jni.cpp
+++ no-jni.cpp
@@ -16,7 +16,15 @@ using namespace std;
 int main(int c, char **v) {
   constexpr auto swt = root_package / "org" / "eclipse" / "swt";
   cout << "SWT package name: " << swt << ";\n";
-  cout << jsignature<int> << ";\n";
+  cout << jsignature<jboolean> << ";\n";
+  cout << jsignature<jbyte> << ";\n";
+  cout << jsignature<jchar> << ";\n";
+  cout << jsignature<jshort> << ";\n";
+  cout << jsignature<jint> << ";\n";
+  cout << jsignature<jlong> << ";\n";
+  cout << jsignature<jfloat> << ";\n";
+  cout << jsignature<jdouble> << ";\n";
+  cout << jsignature<jvoid> << ";\n";
 
   JavaVirtualMachine jvm(c, v);
   cout << jvm.vm << ' ' << jvm.env << '\n';
