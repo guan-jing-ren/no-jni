28cc7c19cc4ba721b93693ff7a772518b1412411
Kwan King Yan <kwan.king.yan@gmail.com>
2017-11-25 16:47:40 +1100

    Handle allocated JVMTI classes with allocated members.

--- no-jvmti.cpp
+++ no-jvmti.cpp
@@ -135,6 +135,8 @@ template <typename J> struct tAlloc {
       std::for_each(j, j + count, [](J &j) {
         if constexpr (std::is_convertible<J, jobject>::value)
           jReference::steal(reinterpret_cast<jobject &>(j));
+        else if constexpr (!std::is_trivially_destructible<J>::value)
+          j.~J();
         else if constexpr (!std::is_same<J, jmethodID>::value &&
                            !std::is_same<J, jfieldID>::value)
           env()->Deallocate(reinterpret_cast<unsigned char *>(j));
@@ -144,6 +146,10 @@ template <typename J> struct tAlloc {
 
   operator jint *() { return &count; }
   operator J **() { return &j; }
+  template <typename JJ> operator JJ **() {
+    static_assert(std::is_convertible<J, JJ>::value && sizeof(J) == sizeof(JJ));
+    return reinterpret_cast<JJ **>(&j);
+  }
 
   J *begin() { return j; }
   J *end() { return j + count; }
