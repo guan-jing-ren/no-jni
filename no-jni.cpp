1b786aad8f1b19ad75f17e7298373d0289145b7f
Kwan King Yan <kwan.king.yan@gmail.com>
2017-12-03 15:58:28 +1100

    Use generated SWT jObjects.

--- no-jni.cpp
+++ no-jni.cpp
@@ -6,17 +6,35 @@ env LD_LIBRARY_PATH=/usr/lib/jvm/default-java/jre/lib/amd64/server/ ./nojni -Dru
 // clang-format on
 
 #include "no-jni.hpp"
-#include "no-swt.hpp"
 
 #include <algorithm>
 #include <iostream>
 #include <vector>
 
+#include "org/eclipse/swt/SWT.jpp"
+#include "org/eclipse/swt/graphics/Device.jpp"
+#include "org/eclipse/swt/graphics/Point.jpp"
+#include "org/eclipse/swt/widgets/Canvas.jpp"
+#include "org/eclipse/swt/widgets/Composite.jpp"
+#include "org/eclipse/swt/widgets/Control.jpp"
+#include "org/eclipse/swt/widgets/Decorations.jpp"
+#include "org/eclipse/swt/widgets/Display.jpp"
+#include "org/eclipse/swt/widgets/Scrollable.jpp"
+#include "org/eclipse/swt/widgets/Shell.jpp"
+#include "org/eclipse/swt/widgets/Widget.jpp"
+
+using org::eclipse::swt::SWT;
+using namespace org::eclipse::swt::widgets;
+using org::eclipse::swt::graphics::Point;
+
+struct PointArray : public jObject<Point[]> {
+  static constexpr auto signature = make_signature<Point[]>{}();
+};
+
 using namespace std;
 
 [[maybe_unused]] constexpr jPackage com{"com"};
 
-int main(int c, char **v) {
   cout << "SWT package name: " << swt_widgets << ";\n";
   cout << jSignature<jboolean> << ";\n";
   cout << jSignature<jbyte> << ";\n";
@@ -33,72 +51,53 @@ int main(int c, char **v) {
   cout << jSignature<IntArrayArray> << "\n";
   cout << Display::method_signatures << "\n";
 
-  JavaVirtualMachine jvm(c, v);
-  cout << jvm.vm << ' ' << jvm.env << "\n";
-
-  std::cout << "Display getCurrent: " << Display::scall<Display>("getCurrent")
-            << "\n";
-  std::cout << "Display getDefault: " << Display::scall<Display>("getDefault")
-            << "\n";
+void basic_test(bool b) {
+  std::cout << "Display getCurrent: " << Display::getCurrent() << "\n";
+  std::cout << "Display getDefault: " << Display::getDefault() << "\n";
   std::cout << "Display getDefault hashCode: "
-            << Display::scall<Display>("getDefault").call<jint>("hashCode")
-            << "\n";
+            << Display::getDefault().call<jint>("hashCode") << "\n";
   std::cout << "Display getDefault getCursorLocation x,y: "
-            << Display::scall<Display>("getDefault")
-                   .call<Point>("getCursorLocation")
-                   .at<jint>("x")
-            << ","
-            << Display::scall<Display>("getDefault")
-                   .call<Point>("getCursorLocation")
-                   .at<jint>("y")
-            << "\n";
+            << Display::getDefault().getCursorLocation().x() << ","
+            << Display::getDefault().getCursorLocation().y() << "\n";
 
-  auto point =
-      Display::scall<Display>("getDefault").call<Point>("getCursorLocation");
+  auto point = Display::getDefault().getCursorLocation();
   auto point2 = point;
-  std::cout << "Point 2: " << point2.at<jint>("x") << ","
-            << point2.at<jint>("y") << "\n";
-  point2.at<jint>("x") = 42;
-  point2.at<jint>("y") = 43;
-  std::cout << "Point: " << point.at<jint>("x") << "," << point.at<jint>("y")
-            << "\n";
-  std::cout << "Point 2: " << point2.at<jint>("x") << ","
-            << point2.at<jint>("y") << "\n";
+  std::cout << "Point 2: " << point2.x() << "," << point2.y() << "\n";
+  point2.x() = 42;
+  point2.y() = 43;
+  std::cout << "Point: " << point.x() << "," << point.y() << "\n";
+  std::cout << "Point 2: " << point2.x() << "," << point2.y() << "\n";
 
-  std::cout << "SWT ALT: " << SWT::sat<jint>("ALT") << "\n";
-  std::cout << "SWT ARROW_UP: " << SWT::sat<jint>("ARROW_UP") << "\n";
-  std::cout << "SWT ARROW_DOWN: " << SWT::sat<jint>("ARROW_DOWN") << "\n";
-  std::cout << "SWT ARROW_LEFT: " << SWT::sat<jint>("ARROW_LEFT") << "\n";
-  std::cout << "SWT ARROW_RIGHT: " << SWT::sat<jint>("ARROW_RIGHT") << "\n";
+  std::cout << "SWT ALT: " << SWT::ALT() << "\n";
+  std::cout << "SWT ARROW_UP: " << SWT::ARROW_UP() << "\n";
+  std::cout << "SWT ARROW_DOWN: " << SWT::ARROW_DOWN() << "\n";
+  std::cout << "SWT ARROW_LEFT: " << SWT::ARROW_LEFT() << "\n";
+  std::cout << "SWT ARROW_RIGHT: " << SWT::ARROW_RIGHT() << "\n";
 
   Display display;
-  std::cout << "Display findWidget: "
-            << display.call<Widget>("findWidget", jlong{}, jlong{}) << "\n";
+  std::cout << "Display findWidget: " << display.findWidget(jlong{}, jlong{})
+            << "\n";
   std::cout << "Display getWidget: "
             << display.call<Widget>("getWidget", jlong{}, jlong{}) << "\n";
 
   std::cout << "Display getClass: " << Display::getClass() << "\n";
 
-  auto icon_sizes =
-      Display::scall<Display>("getDefault").call<PointArray>("getIconSizes");
+  auto icon_sizes = Display::getDefault().call<PointArray>("getIconSizes");
   std::cout << "Icon sizes: " << icon_sizes << "\n";
   auto num_icon_sizes = icon_sizes.size();
   icon_sizes[0] = point;
   std::cout << "Icon sizes size: " << num_icon_sizes << "\n";
   for (Point icon_size : icon_sizes) {
-    std::cout << "Icon size: " << icon_size.at<jint>("x") << ","
-              << icon_size.at<jint>("y") << "\n";
+    std::cout << "Icon size: " << icon_size.x() << "," << icon_size.y() << "\n";
   }
 
   std::vector<Point> icon_size_vector(begin(icon_sizes), end(icon_sizes));
   for (Point icon_size : icon_size_vector) {
-    std::cout << "Icon size: " << icon_size.at<jint>("x") << ","
-              << icon_size.at<jint>("y") << "\n";
+    std::cout << "Icon size: " << icon_size.x() << "," << icon_size.y() << "\n";
   }
 
-  Point pnew(84, point2.at<jint>("y"));
-  std::cout << "pnew: " << pnew.at<jint>("x") << "," << pnew.at<jint>("y")
-            << "\n";
+  Point pnew(84, point2.y());
+  std::cout << "pnew: " << pnew.x() << "," << pnew.y() << "\n";
 
   PointArray snew{icon_sizes.size() + 1};
   std::copy(make_reverse_iterator(end(icon_sizes)),
@@ -107,12 +106,11 @@ int main(int c, char **v) {
   for (Point icon_size : snew) {
     std::cout << "snew Icon size: " << icon_size << "\n";
   }
+}
 
-  Shell shell{Display::scall<Display>("getDefault")};
-  shell.call<jvoid>("setText", String{"NoJNI shell test!"});
-  shell.call<jvoid>("open");
+int main(int c, char **v) {
+  JavaVirtualMachine jvm(c, v);
 
-  while (!shell.call<jboolean>("isDisposed")) {
     auto shells =
         Display::scall<Display>("getDefault").call<ShellArray>("getShells");
     std::cout << "Shells: " << shells << "\n";
@@ -120,11 +118,16 @@ int main(int c, char **v) {
     std::cout << "Shells size: " << num_shells << "\n";
     for (auto i = 0; i < num_shells; ++i)
       std::cout << "Shell " << i << ": " << shells[i] << "\n";
-    if (!Display::scall<Display>("getDefault")
-             .call<jboolean>("readAndDispatch"))
-      Display::scall<Display>("getDefault").call<jboolean>("sleep");
-  }
-  Display::scall<Display>("getDefault").call<jvoid>("dispose");
+
+  Shell shell{Display::getDefault()};
+  shell.setText(String{"NoJNI shell test!"});
+  shell.open();
+
+  while (!shell.call<jboolean>("isDisposed"))
+    if (!Display::getDefault().readAndDispatch())
+      Display::getDefault().sleep();
+
+  Display::getDefault().call<jvoid>("dispose");
 
   return 0;
 }
